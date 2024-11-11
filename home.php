68a1c3a5f68cb7471daf5ce8a74e6e478c277e41
Jefferson Llerena <150809369+jeffdanurss@users.noreply.github.com>
2024-11-11 09:25:28 -0600

    fix: :fire: Message in the web page
    change 1

---
+++ home.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content = "IE-edge">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>WEB PAGE</title>
+</head>
+<body>
+    <h1>Distribuida</h1>
+    <p></p>
+    <?php 
+    echo "<p>Hola mundo distribuida1</p>\n";
+    ?>
+</body>
+</html>
